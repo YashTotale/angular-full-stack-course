f10dcf3d54f7c42b32f244a798775d2617d462e7
Yash Totale <totaleyash@gmail.com>
2021-07-14 13:33:23 -0700

    Add All Pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,14 @@ import { BrowserModule } from "@angular/platform-browser";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { ListingsPageComponent } from "./listings-page/listings-page.component";
+import { ListingDetailPageComponent } from './listing-detail-page/listing-detail-page.component';
+import { ContactPageComponent } from './contact-page/contact-page.component';
+import { MyListingsPageComponent } from './my-listings-page/my-listings-page.component';
+import { NewListingPageComponent } from './new-listing-page/new-listing-page.component';
+import { EditListingPageComponent } from './edit-listing-page/edit-listing-page.component';
 
 @NgModule({
-  declarations: [AppComponent, ListingsPageComponent],
+  declarations: [AppComponent, ListingsPageComponent, ListingDetailPageComponent, ContactPageComponent, MyListingsPageComponent, NewListingPageComponent, EditListingPageComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
