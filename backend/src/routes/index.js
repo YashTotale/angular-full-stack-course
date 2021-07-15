9989e270981bfe0f68967f72523aed69c5bb7b80
Yash Totale <totaleyash@gmail.com>
2021-07-15 11:46:59 -0700

    Add Create New Listing Route

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,10 +1,12 @@
 import { addViewToListingRoute } from "./addViewToListing";
+import { createNewListingRoute } from "./createNewListing";
 import { getAllListingsRoute } from "./getAllListings";
 import { getListingRoute } from "./getListing";
 import { getUserListingsRoute } from "./getUserListings";
 
 export default [
   addViewToListingRoute,
+  createNewListingRoute,
   getAllListingsRoute,
   getListingRoute,
   getUserListingsRoute,
