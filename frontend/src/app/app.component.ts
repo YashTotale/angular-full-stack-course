676a99e87b86798c5edec6b0e529a426ac142dfc
Yash Totale <totaleyash@gmail.com>
2021-07-15 13:05:08 -0700

    Add Sign in & Sign out Functionality

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from "@angular/core";
+import { AngularFireAuth } from "@angular/fire/auth";
+import firebase from "firebase/app";
 
 @Component({
   selector: "app-root",
@@ -7,4 +9,14 @@ import { Component } from "@angular/core";
 })
 export class AppComponent {
   title = "buy-and-sell";
+
+  constructor(public auth: AngularFireAuth) {}
+
+  signInClicked(): void {
+    this.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+  }
+
+  signOutClicked(): void {
+    this.auth.signOut();
+  }
 }
