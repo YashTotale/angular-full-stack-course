eb1045e1ee8d8e584b145289a6fa5616b03de184
Yash Totale <totaleyash@gmail.com>
2021-07-14 21:05:07 -0700

    Update New Listing Page

--- src/app/new-listing-page/new-listing-page.component.ts
+++ src/app/new-listing-page/new-listing-page.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-new-listing-page',
-  templateUrl: './new-listing-page.component.html',
-  styleUrls: ['./new-listing-page.component.css']
+  selector: "app-new-listing-page",
+  templateUrl: "./new-listing-page.component.html",
+  styleUrls: ["./new-listing-page.component.css"],
 })
 export class NewListingPageComponent implements OnInit {
+  name: string = "";
+  description: string = "";
+  price: string = "";
 
-  constructor() { }
+  constructor(private router: Router) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onSubmit(): void {
+    alert(`Creating a new listing...`);
+    this.router.navigateByUrl("/my-listings");
   }
-
 }
