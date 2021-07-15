d8ec99236a4fc40d23ebaba6068b599981098f82
Yash Totale <totaleyash@gmail.com>
2021-07-15 10:42:46 -0700

    Add Hapi Boom

--- backend/src/routes/getListing.js
+++ backend/src/routes/getListing.js
@@ -1,3 +1,4 @@
+import Boom from "@hapi/boom";
 import { fakeListings } from "./fake-data";
 
 export const getListingRoute = {
@@ -5,6 +6,8 @@ export const getListingRoute = {
   path: "/api/listings/{id}",
   handler: (req, h) => {
     const id = req.params.id;
-    return fakeListings.find((listing) => listing.id === id);
+    const listing = fakeListings.find((listing) => listing.id === id);
+    if (!listing) throw Boom.notFound(`Listing does not exist with id ${id}`);
+    return listing;
   },
 };
