fac49128de1b96016acc192914172a7e1fb353f8
Yash Totale <totaleyash@gmail.com>
2021-07-15 12:17:29 -0700

    Add Listings Service

--- frontend/src/app/listing-data-form/listing-data-form.component.ts
+++ frontend/src/app/listing-data-form/listing-data-form.component.ts
@@ -33,6 +33,7 @@ export class ListingDataFormComponent implements OnInit {
       name: this.name,
       description: this.description,
       price: Number(this.price),
+      views: 0,
     });
   }
 }
---
+++ frontend/src/app/listings.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from "@angular/core";
+import { fakeListings } from "./fake-data";
+import { Listing } from "./types";
+
+@Injectable({
+  providedIn: "root",
+})
+export class ListingsService {
+  constructor() {}
+
+  getListings(): Listing[] {
+    return fakeListings;
+  }
+}
