eabd66d6ed724b4d60840b80bfa2570f9b2b87d4
Yash Totale <totaleyash@gmail.com>
2021-07-15 13:12:48 -0700

    Load User Listings with Auth

--- backend/src/routes/getUserListings.js
+++ backend/src/routes/getUserListings.js
@@ -1,10 +1,18 @@
+import * as admin from "firebase-admin";
+import Boom from "@hapi/boom";
 import { db } from "../database";
 
 export const getUserListingsRoute = {
   method: "GET",
   path: "/api/users/{userId}/listings",
   handler: async (req, h) => {
+    const token = request.headers.authtoken;
+    const user = await admin.auth().verifyIdToken(token);
     const userId = req.params.userId;
+
+    if (user.uid !== userId)
+      throw Boom.unauthorized("Users can only access their own listings!");
+
     const { results } = await db.query(
       "SELECT * FROM LISTINGS WHERE user_id=?",
       [userId]
