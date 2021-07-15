4c92d6087ba6d8aae950a67002d941ddf6d621b0
Yash Totale <totaleyash@gmail.com>
2021-07-15 09:59:14 -0700

    Use Listing Data Form Component in Edit Listing & New Listing Pages

--- src/app/edit-listing-page/edit-listing-page.component.ts
+++ src/app/edit-listing-page/edit-listing-page.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-edit-listing-page',
-  templateUrl: './edit-listing-page.component.html',
-  styleUrls: ['./edit-listing-page.component.css']
+  selector: "app-edit-listing-page",
+  templateUrl: "./edit-listing-page.component.html",
+  styleUrls: ["./edit-listing-page.component.css"],
 })
 export class EditListingPageComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onSubmit(): void {
+    alert(`Saving changes to the listing...`);
+    this.router.navigateByUrl("/my-listings");
   }
-
 }
--- src/app/listing-data-form/listing-data-form.component.ts
+++ src/app/listing-data-form/listing-data-form.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
 import { Router } from "@angular/router";
+import { Listing } from "../types";
 
 @Component({
   selector: "app-listing-data-form",
@@ -7,16 +8,23 @@ import { Router } from "@angular/router";
   styleUrls: ["./listing-data-form.component.css"],
 })
 export class ListingDataFormComponent implements OnInit {
+  @Input() buttonText;
   name: string = "";
   description: string = "";
   price: string = "";
+
+  @Output() onSubmit = new EventEmitter<Listing>();
 
   constructor(private router: Router) {}
 
   ngOnInit(): void {}
 
-  onSubmit(): void {
-    alert(`Creating a new listing...`);
-    this.router.navigateByUrl("/my-listings");
+  onButtonClicked(): void {
+    this.onSubmit.emit({
+      id: null,
+      name: this.name,
+      description: this.description,
+      price: Number(this.price),
+    });
   }
 }
