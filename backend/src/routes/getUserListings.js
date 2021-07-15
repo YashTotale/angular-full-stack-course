47fd8bc1fe3c7f4da12800e6f8603792d4d6fe30
Yash Totale <totaleyash@gmail.com>
2021-07-15 11:38:46 -0700

    Add Get User Listings Route

---
+++ backend/src/routes/getUserListings.js
@@ -0,0 +1,14 @@
+import { db } from "../database";
+
+export const getUserListingsRoute = {
+  method: "GET",
+  path: "/api/users/{userId}/listings",
+  handler: async (req, h) => {
+    const userId = req.params.userId;
+    const { results } = await db.query(
+      "SELECT * FROM LISTINGS WHERE user_id=?",
+      [userId]
+    );
+    return results;
+  },
+};
