5d59f78d6d34a533ebfd9e86ee3dafa15688d2dd
Yash Totale <totaleyash@gmail.com>
2021-07-14 20:54:21 -0700

    Update Contact Page

--- src/app/contact-page/contact-page.component.ts
+++ src/app/contact-page/contact-page.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+import { fakeListings } from "../fake-data";
+import { Listing } from "../types";
 
 @Component({
-  selector: 'app-contact-page',
-  templateUrl: './contact-page.component.html',
-  styleUrls: ['./contact-page.component.css']
+  selector: "app-contact-page",
+  templateUrl: "./contact-page.component.html",
+  styleUrls: ["./contact-page.component.css"],
 })
 export class ContactPageComponent implements OnInit {
+  email: string = "";
+  message: string = "";
+  listing: Listing;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private router: Router) {}
 
   ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get("id");
+    this.listing = fakeListings.find((listing) => listing.id === id);
+    this.message = `Hi, I'm interested in your ${this.listing.name.toLowerCase()}!`;
   }
 
+  sendMessage(): void {
+    alert("Your message has been sent!");
+    this.router.navigateByUrl("/listings");
+  }
 }
